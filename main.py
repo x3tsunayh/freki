8bc4392afd3acb51b56cad7c1c4694eb20a5f00d
chuayupeng <chua.yupeng@dhs.sg>
2018-12-08 16:26:49 +0800

    added injector module

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import argparse
 import crawler
+import injector
 
 def main(args):
-    crawler.crawl(args.url, args.payload, args.keyword, args.cookie)
+    if args.inject:
+        injector.inject(args.url, args.payload, args.keyword, args.cookie)
+    else:
+        crawler.crawl(args.url, args.payload, args.keyword, args.cookie)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -10,5 +14,6 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--payload", help="Malicious script to submit as payload", type=str, required=True)
     parser.add_argument("-c", "--cookie", help="For user identifcation purposes", type=str, required=False)
     parser.add_argument("-k", "--keyword", help="Restrict search within the domain of a keyword", type=str, required=False)
+    parser.add_argument("-i", "--inject", help="Toggle on Injection mode", action='store_true', required=False)
     args = parser.parse_args()
     main(args)
